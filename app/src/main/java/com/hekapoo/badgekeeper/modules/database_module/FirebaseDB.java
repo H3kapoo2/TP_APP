9b4d9a2ab3f9521b61fe3d2c3779378516c3d5d7
H3kapoo <andrei.boje00@e-uvt.ro>
2021-04-21 20:15:12 +0300

    Heka: data now appears in the dashboard

--- app/src/main/java/com/hekapoo/badgekeeper/modules/database_module/FirebaseDB.java
+++ app/src/main/java/com/hekapoo/badgekeeper/modules/database_module/FirebaseDB.java
@@ -3,9 +3,18 @@ package com.hekapoo.badgekeeper.modules.database_module;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.hekapoo.badgekeeper.modules.utils_module.UserSchema;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  * Class handling firebase database manipulation
@@ -27,6 +36,10 @@ public class FirebaseDB {
         void onComplete(Boolean value);
     }
 
+    public interface callback2 {
+        void onComplete(UserSchema value);
+    }
+
     public void registerNewUser(String email, String password, UserSchema schema, callback result) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
 
@@ -35,10 +48,9 @@ public class FirebaseDB {
             if (task.isSuccessful()) {
                 FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(schema).addOnCompleteListener(task1 -> {
 
-                    if (task1.isSuccessful()){
+                    if (task1.isSuccessful()) {
                         result.onComplete(task1.isSuccessful());
-                    }
-                    else
+                    } else
                         Log.d("FB", "toDB_STORE: " + task1.getException().getMessage());
                 });
 
@@ -49,22 +61,50 @@ public class FirebaseDB {
 
     //true if email corresponds to a user
     //false otherwise
-    public void emailInUse(String email,callback result){
+    public void emailInUse(String email, callback result) {
 
         FirebaseAuth.getInstance().fetchSignInMethodsForEmail(email).addOnCompleteListener(task -> {
             //check if the email is not in the DB
-            if(task.getResult().getSignInMethods().isEmpty())
+            if (task.getResult().getSignInMethods().isEmpty())
                 result.onComplete(false);
             else
                 result.onComplete(true);
         });
     }
 
-    public void loginUser(String email,String password,callback result){
+    public void loginUser(String email, String password, callback result) {
 
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password).addOnCompleteListener(task->{
-           result.onComplete(task.isSuccessful());
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            result.onComplete(task.isSuccessful());
         });
     }
 
+    public void getUserFirebase(callback2 result) {
+
+        DatabaseReference firebaseDatabase;
+        firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+
+        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        firebaseDatabase.child("Users").child(userID).get().addOnCompleteListener(task -> {
+
+            Map<String, String> userMap;
+
+            if (task.isSuccessful()) {
+                UserSchema user;
+                userMap = (Map<String, String>) task.getResult().getValue();
+
+                if (!userMap.isEmpty()) {
+                    result.onComplete(new UserSchema(userMap.get("email"),
+                            userMap.get("localization"),
+                            userMap.get("department"),
+                            userMap.get("cardID"),
+                            userMap.get("cardNumber")));
+                    Log.d("firebase_read_data", userMap.toString());
+                }
+            } else {
+                result.onComplete(null);
+                Log.e("firebase_read_data", "Error getting data", task.getException());
+            }
+        });
+    }
 }
--- app/src/main/java/com/hekapoo/badgekeeper/ui_drivers/dashboard_driver/DashboardUIDriver.java
+++ app/src/main/java/com/hekapoo/badgekeeper/ui_drivers/dashboard_driver/DashboardUIDriver.java
@@ -1,24 +1,73 @@
 package com.hekapoo.badgekeeper.ui_drivers.dashboard_driver;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 import com.hekapoo.badgekeeper.R;
 import com.hekapoo.badgekeeper.modules.database_module.LocalDatabase;
 import com.hekapoo.badgekeeper.modules.utils_module.UserSchema;
 
 public class DashboardUIDriver extends AppCompatActivity {
 
+    TextView firstNameTV,lastNameTV,localizationTV;
+    TextView workGoalTV,workDoneTV;
+    CardView activityIm,peopleIm,profileIm,settingsIm;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_dashboard);
 
-        UserSchema user = LocalDatabase.getInstance().loadUserLocally(this);
+        firstNameTV = findViewById(R.id.badge_name1);
+        lastNameTV = findViewById(R.id.badge_name2);
+        localizationTV = findViewById(R.id.badge_localization);
 
-        Log.d("user", "onCreate: " + (user == null));
+        workGoalTV = findViewById(R.id.bagde_goal);
+        workDoneTV = findViewById(R.id.badge_current_time);
+
+        activityIm = findViewById(R.id.activity_im);
+        peopleIm = findViewById(R.id.people_im);
+        profileIm = findViewById(R.id.profile_im);
+        settingsIm = findViewById(R.id.settings_im);
+
+        UserSchema user = LocalDatabase.getInstance().loadUserLocally(this);
+        Log.d("user", "onCreate : " + (user == null) + " " + user.email);
+
+        //todo: validate email to be of the form xxx.xxxx@nokia.com ONLY
+        //this should pass for now ^
+        String firstName = user.getEmail().split("@")[0].split("\\.")[0].toUpperCase();
+        String lastName = user.getEmail().split("@")[0].split("\\.")[1].toUpperCase();
+
+        firstNameTV.setText(firstName);
+        lastNameTV.setText(lastName);
+        localizationTV.setText(user.getLocalization());
+
+        activityIm.setOnClickListener(e->{
+            Intent intent = new Intent(this,ActivityUIDriver.class);
+            startActivity(intent);
+        });
+
+        peopleIm.setOnClickListener(e->{
+            Intent intent = new Intent(this,PeopleUIDriver.class);
+            startActivity(intent);
+        });
+
+        profileIm.setOnClickListener(e->{
+            Intent intent = new Intent(this,ProfileUIDriver.class);
+            startActivity(intent);
+        });
+
+        settingsIm.setOnClickListener(e->{
+            Intent intent = new Intent(this,SettingsUIDriver.class);
+            startActivity(intent);
+        });
+
     }
 }
